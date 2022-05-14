e4674bd0105d4ea42dcc24a0b0e16604c8eb09d6
holomdev <holom.dev@gmail.com>
2022-05-14 12:37:12 -0500

    chore: package update

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
 export default {
   clearMocks: true,
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   roots: ['<rootDir>/src'],
   testEnvironment: 'node',
   transform: {
--- src/controllers/IndexController.ts
+++ src/controllers/IndexController.ts
@@ -1,10 +1,13 @@
 import { NextFunction, Request, Response, Router } from 'express'
 export const IndexController: Router = Router()
 
-IndexController.get('/', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    res.status(200).send({ data: 'Hello from Node!' })
-  } catch (e) {
-    next(e)
+IndexController.get(
+  '/',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.status(200).send({ data: 'Hello from Node!' })
+    } catch (e) {
+      next(e)
+    }
   }
-})
+)
